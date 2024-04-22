2fa8137a456a1be1305828e855b813a2e8e05ee7
Mitsuharu Emoto <m_emoto@yumemi.co.jp>
2024-04-22 10:27:06 +0900

    記事順番を修正

--- book/vivliostyle.config.js
+++ book/vivliostyle.config.js
@@ -12,13 +12,19 @@ module.exports = {
     'index.md',
     // はじめに
     'preface.md',
+
     // 各章の原稿
-    'omoriLt.md',
-    'emoto.md',
     'usami.md',
-    'kawashima.md',
+    'emoto.md',
     'iseki.md',
+    // K9iさん予定
+    'omoriLt.md',
     'hajimism-feedback-philosophy.md',
+    'kawashima.md',
+    // 栗山さん予定
+    // 菅原さん予定
+    // 村石さん予定
+
     // 著者紹介
     'authors.md',
     // 奥付
